fb166e2b49872c7e4e196f77be9b6158914f2090
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-09-29 15:46:44 +0200

    Übung 8 läuft

--- src/app/shared/book-data.service.ts
+++ src/app/shared/book-data.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
 import {Book} from "./book";
 import {Observable} from "rxjs";
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BookDataService  {
 
-  books: Book[] = [ //also possible: Array<Book>
-    {title: 'Design Patterns', isbn: '978-0-20163-361-0', author: 'Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides'},
-    {title: 'REST und HTTP', isbn: '978-3-86490-120-1', author: 'Stefan Tilkov / Martin Eigenbrodt / Silvia Schreier / Oliver Wolf'},
-    {title: 'Eloquent JavaScript', isbn: '978-1-59327-584-6', author: 'Marijn Haverbeke'}
-  ];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getBooks(): Observable<Book[]> {
-    return Observable.of(this.books);
+    return this.http.get('http://localhost:4730/books')
+      .map(response => response.json());
   }
 
 }
