b37750347911391da7ff1ea3bbc6aad81454a6af
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-09-29 16:11:30 +0200

    Übung 9 läuft

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,32 +1,35 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, OnDestroy} from '@angular/core';
 import {BookDataService} from "../shared/book-data.service";
 import {Book} from "../shared/book";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
   name:           string;
   x:              number;
   y:              number;
   @Input() title: string;
   @Output() titleClicked = new EventEmitter<MouseEvent>();
   books: Book[] = [];
+  subscriber: Subscription;
 
   constructor(private bookDataService: BookDataService) {
+  }
+
+  ngOnInit(): void {
+    console.log('I am in onInit');
     this.name = 'Christian';
     this.x = 0;
     this.y = 0;
-    bookDataService.getBooks().subscribe((books: Book[]) =>  {
-       this.books = books;
-     });
+    this.subscriber = this.bookDataService.getBooks().subscribe((books: Book[]) =>  {
+      this.books = books;
+    });
   }
 
-  ngOnInit() {
-    console.log('I am in onInit');
-  }
 
   mouseMove(event: MouseEvent): void {
     this.x = event.offsetX;
@@ -35,5 +38,9 @@ export class BookListComponent implements OnInit {
 
   sendTitleClicked(event: MouseEvent): void {
     this.titleClicked.emit(event);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
   }
 }
