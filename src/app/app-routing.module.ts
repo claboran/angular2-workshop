2f3496f00d0cb69a19fc8885450c8a74018a68eb
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-09-30 09:23:37 +0200

    Übung 12 läuft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BookListComponent} from "./book-list/book-list.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {path: 'books', component: BookListComponent},
-  {path: '**', component: PageNotFoundComponent}
+  {path: '', pathMatch: 'full', redirectTo: '/books'}
 ];
 
 @NgModule({
