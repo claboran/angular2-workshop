ba4f3bd8347771122d638141bad80776af9e35b6
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-09-30 16:12:07 +0200

    Übung 19 läuft

--- src/app/book/book-edit/book-edit.component.ts
+++ src/app/book/book-edit/book-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Book} from "../shared/book";
+import {ActivatedRoute} from "@angular/router";
+import {BookDataService} from "../shared/book-data.service";
 
 @Component({
   selector: 'book-edit',
@@ -10,9 +12,16 @@ export class BookEditComponent implements OnInit {
 
   public book: Book = {isbn: '', author: '', title: ''};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private bookDataService: BookDataService) { }
 
   ngOnInit() {
+    this.route
+      .params
+      .mergeMap((params: {isbn: string}) => this.bookDataService.getBooksByIsbn(params.isbn))
+      .subscribe((book: Book) => this.book = book);
   }
 
+  onSubmit(form: any): void {
+    console.log('Form: ', form);
+  }
 }
